c4531b0373b2e946c5405d9514f0bcc9aa9859e5
yurigregorio <yuri.gregorio1@hotmail.com>
2021-11-29 21:12:50 -0300

    Atualização para ajustar erro de CORS em Produtos

--- src/main/java/br/com/istorage/controller/ProdutoController.java
+++ src/main/java/br/com/istorage/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,6 +18,7 @@ import br.com.istorage.model.Produto;
 import br.com.istorage.service.ProdutoService;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/produtos")
 public class ProdutoController {
 
