a9c8e009f1ca256b1b2539069403d3cff8db98c7
yurigregorio <yuri.gregorio1@hotmail.com>
2021-11-29 21:39:01 -0300

    Update cors

--- src/main/java/br/com/istorage/config/CorsConfig.java
+++ src/main/java/br/com/istorage/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("https://alphadev-project.herokuapp.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
